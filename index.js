147f5860ebe2d06d4b3222a6beff258dffa64cc2
Aleemath <aleemathranseenakk@gmail.com>
2024-10-05 22:57:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+//multiple products
+// app.get("/api/products", async (req, res) => {
+//   try {
+//     const products = await Product.find({});
+//     res.status(200).json(products);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+//single product
+// app.get("/api/products/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findById(id);
+//     res.status(200).json(product);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+//update a product
+// app.put("/api/products/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findByIdAndUpdate(id, req.body);
+//     if (!product) {
+//       return res.status(404).json({ message: "Product not Found" });
+//     }
+//     const updatedProduct = await Product.findById(id);
+//     res.status(200).json(updatedProduct);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+//delete a product
+// app.delete("/api/products/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const product = await Product.findByIdAndDelete(id, req.body);
+//     if (!product) {
+//       return res.status(404).json({ message: "Product not Found" });
+//     }
+
+//     res.status(200).json("Product deleted successfully");
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+//add products
+// app.post("/api/products", async (req, res) => {
+//   try {
+//     const product = await Product.create(req.body);
+//     res.status(200).json(product);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
+
+mongoose
+  .connect(
+    "mongodb+srv://aleemathranseena:4TQGCcrS5I2uq4js@cluster0.mlir2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
